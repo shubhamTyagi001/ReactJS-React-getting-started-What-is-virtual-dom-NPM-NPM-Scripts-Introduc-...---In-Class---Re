63fd5a592a2cde9f4d8f0db32ee55f712bf1ceb1
shubhamTyagi001 <91019986+shubhamTyagi001@users.noreply.github.com>
2022-05-29 17:14:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,13 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <div data-ns-test="project-name">Amazon</div>
+      <div data-ns-test="project-description">Ecommerce</div>
+
+      <div data-ns-test="project-name">Google</div>
+      <div data-ns-test="project-description">Search Engine</div>
+    </div>
   )
 }
 
